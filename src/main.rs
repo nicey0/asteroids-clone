9f2845b07c21357e1d8261d468c2edff7265a209
nicey <nicey_pm@protonmail.com>
2020-11-09 22:26:24 -0300

    ebic speed and stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::f64::consts::PI;
+
+const WIDTH: u32 = 600;
+const HEIGHT: u32 = 600;
+const RADTODEG: f64 = 2.0 * (PI / 360.0);
+
+#[derive(Debug)]
+struct Ship {
+    x: f64,
+    y: f64,
+    spd: f64,
+    rot: f64,
+}
+
+impl Ship {
+    fn new() -> Self {
+        Self {
+            x: 0.0,
+            y: 0.0,
+            spd: 0.0,
+            rot: 0.0,
+        }
+    }
+
+    fn rotate(&mut self, deg: f64) {
+        self.rot += deg;
+    }
+
+    fn set_speed(&mut self, spd: f64) {
+        self.spd = spd;
+    }
+
+    fn tick(&mut self) {
+        self.x += self.spd * (self.rot * RADTODEG).cos();
+        self.y += self.spd * (self.rot * RADTODEG).sin();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut ship = Ship::new();
+    ship.rotate(90.0);
+    ship.set_speed(5.0);
+    ship.tick();
+    println!("{:#?}", ship);
+    ship.tick();
+    println!("{:#?}", ship);
+    ship.tick();
+    println!("{:#?}", ship);
+    ship.rotate(-90.0);
+    ship.tick();
+    println!("{:#?}", ship);
 }
