490cb1633ada6e8a5f5ea75c04b6e8a80c4d5b6c
nicey <nicey_pm@protonmail.com>
2020-11-12 09:03:40 -0300

    inside_circle to col.rs

--- src/col.rs
+++ src/col.rs
@@ -1,3 +1,4 @@
+use super::asteroid::Asteroid;
 use super::math::*;
 use super::util::*;
 
@@ -6,6 +7,16 @@ enum Ori {
     Collinear,
     Clock,
     Anticlock,
+}
+
+fn inside_circle(x: f64, y: f64, ast: &Asteroid) -> bool {
+    let sx = ast.get_x();
+    let sy = ast.get_y();
+    let sr = ast.get_r();
+    if ((x - sx).powf(2.0) + (y - sy).powf(2.0)).powf(0.5) <= sr {
+        return true;
+    }
+    false
 }
 
 pub fn point_in_polygon(p1: &APoint, poly: &Vec<APoint>) {}
--- src/math.rs
+++ src/math.rs
@@ -11,14 +11,3 @@ pub fn cos_math(m: f64, rot: f64) -> f64 {
 pub fn sin_math(m: f64, rot: f64) -> f64 {
     m * (rot * RADTODEG).sin()
 }
-
-// collision math
-fn inside_circle(x: f64, y: f64, ast: &Asteroid) -> bool {
-    let sx = ast.get_x();
-    let sy = ast.get_y();
-    let sr = ast.get_r();
-    if ((x - sx).powf(2.0) + (y - sy).powf(2.0)).powf(0.5) <= sr {
-        return true;
-    }
-    false
-}
